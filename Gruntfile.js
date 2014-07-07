abe0817e81a779a14824865b8b23eff64db35169
Ruben Vermeersch <ruben@rocketeer.be>
2014-07-07 16:22:48 +0200

    Update jshint/jscs.

--- Gruntfile.js
+++ Gruntfile.js
@@ -42,7 +42,7 @@ module.exports = function (grunt) {
                 files: {
                     src: ['*.js', '{src,test}/**/*.js']
                 }
-            },
+            }
         },
 
         concat: {
